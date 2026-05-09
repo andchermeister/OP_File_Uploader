984930dc3c81dd0e700dc7f07fe938eb3e70405b
Andrii Chernikov <99753757+andchermeister@users.noreply.github.com>
2026-05-09 22:00:51 +0100

    Made a setup for prisma

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,14 @@ import session from "express-session";
 import passport from "passport";
 import dotenv from "dotenv";
 import mainRouter from "./routes/mainRouter";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
